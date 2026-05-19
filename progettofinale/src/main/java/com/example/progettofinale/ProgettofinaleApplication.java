2516c230d6712625aaf40cadf42837871bdb76a4
TureCatedu <cataudellandrea@gmail.com>
2026-05-19 14:30:43 +0200

    upload spring project

---
+++ progettofinale/src/main/java/com/example/progettofinale/ProgettofinaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettofinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettofinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettofinaleApplication.class, args);
+	}
+
+}
