cfc2fd08b02936aed38ab0de6364a871922554e7
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-05-19 17:50:38 +0200

    Update ErrorResponse.java

--- progettofinale/src/main/java/com/example/progettofinale/errorResponse/ErrorResponse.java
+++ progettofinale/src/main/java/com/example/progettofinale/errorResponse/ErrorResponse.java
@@ -1,7 +1,7 @@
 package com.example.progettofinale.errorResponse;
 
 import java.time.LocalDateTime;
-
+//record per response error
 public record ErrorResponse(
     int status, 
     String message,
