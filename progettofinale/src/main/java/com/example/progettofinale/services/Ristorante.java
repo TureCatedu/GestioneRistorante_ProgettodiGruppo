73e01817b4d3e99cceac99855b81d76593697728
TureCatedu <cataudellandrea@gmail.com>
2026-05-19 16:59:19 +0200

    add prenotazioneController

--- progettofinale/src/main/java/com/example/progettofinale/services/Ristorante.java
+++ progettofinale/src/main/java/com/example/progettofinale/services/Ristorante.java
@@ -1,5 +1,6 @@
 package com.example.progettofinale.services;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,45 +15,55 @@ import com.example.progettofinale.repository.PrenotazioneRepo;
 
 @Service
 public class Ristorante implements Subject {
-    private final  PrenotazioneRepo prenotazioneRepo;
+    private final PrenotazioneRepo prenotazioneRepo;
     Notificatore notificatore;
-    //costruttore con parametri per l'injection
+
+    // costruttore con parametri per l'injection
     public Ristorante(PrenotazioneRepo prenotazioneRepo, Notificatore notificatore) {
         this.prenotazioneRepo = prenotazioneRepo;
         this.notificatore = notificatore;
     }
+
     List<Observer> observers = new ArrayList<>();
+
     @Override
     public void registerObserver(Observer o) {
         observers.add(o);
     }
+
     @Override
     public void removeObserver(Observer o) {
         observers.remove(o);
     }
+
     @Override
     public void notifyObservers(Notifica notifica) {
         for (Observer o : observers) {
             o.update(notifica);
         }
     }
-    //PrenotazioneRequest to Prenotazione
+
+    // PrenotazioneRequest to Prenotazione
     Prenotazione prenotazione(PrenotazioneRequest prenotazioneRequest) {
-        return new Prenotazione(prenotazioneRequest.nomeCliente(), prenotazioneRequest.numeroPersone(), prenotazioneRequest.dataOra());
-    }
-    //Prenotazione to NotificaResponse
-    PrenotazioneResponse prenotazione(Prenotazione prenotazione) {
-        return new PrenotazioneResponse(prenotazione.getId(), prenotazione.getNomeCliente(), prenotazione.getNumeroPersone(), prenotazione.getDataOra());
+        return new Prenotazione(prenotazioneRequest.nomeCliente(), prenotazioneRequest.numeroPersone(),
+                prenotazioneRequest.dataOra());
     }
 
-    //aggiungi prenotazione
+    // Prenotazione to NotificaResponse
+    PrenotazioneResponse prenotazione(Prenotazione prenotazione) {
+        return new PrenotazioneResponse(prenotazione.getId(), prenotazione.getNomeCliente(),
+                prenotazione.getNumeroPersone(), prenotazione.getDataOra());
+    }
+
+    // aggiungi prenotazione
     public PrenotazioneResponse aggiungiPrenotazione(PrenotazioneRequest prenotazioneRequest) {
         Prenotazione prenotazione = prenotazione(prenotazioneRequest);
         Prenotazione prenotazioneDb = prenotazioneRepo.save(prenotazione);
         notifyObservers(new Notifica(prenotazioneDb, "Prenotazione inserita"));
         return prenotazione(prenotazioneDb);
     }
-    //ottieni tutte prenotazioni
+
+    // ottieni tutte prenotazioni
     public List<PrenotazioneResponse> getPrenotazioni() {
         List<Prenotazione> prenotazioni = prenotazioneRepo.findAll();
         List<PrenotazioneResponse> prenotazioniResponse = new ArrayList<>();
@@ -61,7 +72,8 @@ public class Ristorante implements Subject {
         }
         return prenotazioniResponse;
     }
-    //cerca prenotazione
+
+    // cerca prenotazione
     public PrenotazioneResponse cercaPrenotazione(Integer id) {
         Prenotazione prenotazione = prenotazioneRepo.findById(id).orElse(null);
         if (prenotazione == null) {
@@ -69,7 +81,36 @@ public class Ristorante implements Subject {
         }
         return prenotazione(prenotazione);
     }
-    //elimina prenotazione
+
+    // cerca prenotazione per data
+    public List<PrenotazioneResponse> cercaPrenotazionePerData(String dataString) {
+
+        LocalDate data = LocalDate.parse(dataString);
+        
+        List<Prenotazione> prenotazioni = prenotazioneRepo.findByDataOnly(data);
+        List<PrenotazioneResponse> response = new ArrayList<>();
+        
+        for (Prenotazione p : prenotazioni) {
+            response.add(prenotazione(p));
+        }
+        return response;
+    }
+    
+    //cerca prenotazione per giorno corrente
+    public List<PrenotazioneResponse> getPrenotazioniOggi() {
+    
+        LocalDate oggi = LocalDate.now();
+
+        List<Prenotazione> prenotazioni = prenotazioneRepo.findByDataOnly(oggi);
+        List<PrenotazioneResponse> response = new ArrayList<>();
+
+        for (Prenotazione p : prenotazioni) {
+            response.add(prenotazione(p));
+        }
+        return response;
+    }
+
+    // elimina prenotazione
     public void eliminaPrenotazione(Integer id) {
         Prenotazione prenotazione = prenotazioneRepo.findById(id).orElse(null);
         if (prenotazione == null) {
@@ -78,8 +119,9 @@ public class Ristorante implements Subject {
         prenotazioneRepo.delete(prenotazione);
         notifyObservers(new Notifica(prenotazione, "Prenotazione eliminata"));
     }
-    //modifica prenotazione
-    public PrenotazioneResponse modificaPrenotazione(Integer id,PrenotazioneRequest prenotazioneRequest) {
+
+    // modifica prenotazione
+    public PrenotazioneResponse modificaPrenotazione(Integer id, PrenotazioneRequest prenotazioneRequest) {
         Optional<Prenotazione> prenotazione = prenotazioneRepo.findById(id);
         if (prenotazione == null) {
             return null;
@@ -92,7 +134,8 @@ public class Ristorante implements Subject {
         notifyObservers(new Notifica(updated, "Prenotazione modificata"));
         return prenotazione(updated);
     }
-    //cerca per nome e cognome cliente
+
+    // cerca per nome e cognome cliente
     public List<PrenotazioneResponse> cercaPrenotazionePerNomeCliente(String nome, String cognome) {
         List<Prenotazione> prenotazioni = prenotazioneRepo.findByCliente_NomeAndCliente_Cognome(nome, cognome);
         List<PrenotazioneResponse> prenotazioniResponse = new ArrayList<>();
@@ -101,5 +144,5 @@ public class Ristorante implements Subject {
         }
         return prenotazioniResponse;
     }
-    
+
 }
