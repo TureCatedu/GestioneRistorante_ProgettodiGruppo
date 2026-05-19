a9cfee756d263c0d4fb7e4cc9265cde04f45ccc5
BilardoMauro02 <maurobilardo02@gmail.com>
2026-05-19 15:32:03 +0200

    add @Repository annotation to NotificatoreRepo and PrenotazioneRepo interfaces

--- progettofinale/src/main/java/com/example/progettofinale/repository/NotificatoreRepo.java
+++ progettofinale/src/main/java/com/example/progettofinale/repository/NotificatoreRepo.java
@@ -1,9 +1,11 @@
 package com.example.progettofinale.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.progettofinale.models.Notifica;
 
+@Repository
 public interface NotificatoreRepo extends JpaRepository<Notifica, Integer>{
     
 }
