47c78acbeb22051408e81add055ffae257cd5229
BilardoMauro02 <maurobilardo02@gmail.com>
2026-05-19 14:52:30 +0200

    add constructors and getter/setter methods to Prenotazione class

--- progettofinale/target/classes/com/example/progettofinale/models/Prenotazione.java
+++ progettofinale/target/classes/com/example/progettofinale/models/Prenotazione.java
@@ -4,4 +4,38 @@ public class Prenotazione{
     private String nomeCliente;
     private int numeroPersone;
     private LocalDateTime dataOra;
+
+    public Prenotazione() {
+    }
+
+    public Prenotazione(String nomeCliente, int numeroPersone, LocalDateTime dataOra) {
+        this.nomeCliente = nomeCliente;
+        this.numeroPersone = numeroPersone;
+        this.dataOra = dataOra;
+    }
+    
+    public String getNomeCliente() {
+        return this.nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public int getNumeroPersone() {
+        return this.numeroPersone;
+    }
+
+    public void setNumeroPersone(int numeroPersone) {
+        this.numeroPersone = numeroPersone;
+    }
+
+    public LocalDateTime getDataOra() {
+        return this.dataOra;
+    }
+
+    public void setDataOra(LocalDateTime dataOra) {
+        this.dataOra = dataOra;
+    }
+
 }
