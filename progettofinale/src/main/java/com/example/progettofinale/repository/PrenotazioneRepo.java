8a9fafcf7076902eb4011d31c1e5b2df645e690a
BilardoMauro02 <maurobilardo02@gmail.com>
2026-05-19 15:20:06 +0200

    add NotificatoreRepo and PrenotazioneRepo interfaces

---
+++ progettofinale/src/main/java/com/example/progettofinale/repository/PrenotazioneRepo.java
@@ -0,0 +1,18 @@
+package com.example.progettofinale.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.progettofinale.models.Prenotazione;
+import java.util.List;
+import java.time.LocalDateTime;
+
+
+
+public interface PrenotazioneRepo extends JpaRepository<Prenotazione,Integer>{
+    
+    List<Prenotazione> findByNomeCliente(String nomeCliente);
+
+    List<Prenotazione> findByNumeroPersone(int numeroPersone);
+
+    List<Prenotazione> findByDataOra(LocalDateTime dataOra);
+}
